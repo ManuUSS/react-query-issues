9c571e6acb8ab7af53fa622900856f0ec967ccfb
Manuel Ulate <fr3zs09@gmail.com>
2023-09-24 20:02:10 -0600

    labels types

---
+++ src/issues/interfaces/label.ts
@@ -0,0 +1,11 @@
+
+export interface ILabel {
+    id:           number;
+    node_id:      string;
+    url:          string;
+    name:         string;
+    color:        string;
+    default:      boolean;
+    description?: string;
+}
+
