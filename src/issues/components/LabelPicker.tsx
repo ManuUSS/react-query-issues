32b60bda61a7ab95b096100af8170664d882c0e1
Manuel Ulate <fr3zs09@gmail.com>
2023-09-25 11:47:07 -0600

    new: label-active css

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -20,8 +20,9 @@ export const LabelPicker:FC<Props> = ({ selectedLabels, onChange }) => {
         labelsQuery.data?.map(({ id, color, name }) => (
           <span
             key={ id }
-            className="badge rounded-pill m-1 label-picker"
+            className={`badge rounded-pill m-1 label-picker ${ selectedLabels.includes( name ) && 'label-active' }`}
             style={{ border: `1px solid #${ color }`, color: `#${ color }` }}
+            onClick={() => onChange( name )}
           >
             { name }
           </span>
