83658dcaf7e2c4f07da7ae74d763441dfd53ef5e
Manuel Ulate <fr3zs09@gmail.com>
2023-09-24 20:14:19 -0600

    sleep implemented on getLabels

--- src/issues/hooks/useLabels.ts
+++ src/issues/hooks/useLabels.ts
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { githubApi } from '../../api/githubApi';
 import { ILabel } from '../interfaces/label';
+import { sleep } from '../../helpers/sleep';
 
 const getLabels = async ():Promise<ILabel[]> => {
+    await sleep( 2 );
     const { data } = await githubApi<ILabel[]>('/labels');
     console.log( data );
     return data;
