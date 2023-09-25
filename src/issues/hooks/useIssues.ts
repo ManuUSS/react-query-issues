952afd9b836ae006f3387b5e94205966eee43683
Manuel Ulate <fr3zs09@gmail.com>
2023-09-25 12:09:23 -0600

    useIssues custom hook

--- src/issues/hooks/useIssues.ts
+++ src/issues/hooks/useIssues.ts
@@ -1,17 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
 import { IIssue } from '../interfaces';
 
 
 const getIssues = async ():Promise<IIssue[]> => {
 
+    const { data } = await githubApi.get<IIssue[]>('/issues')
 
+    return data;
 
 }
 
 
 export const useIssues = () => {
 
+    const issuesQuery = useQuery(
+        ['issues'],
+        getIssues
+    )
 
-
-
+    return {
+        issuesQuery
+    }
 
 }
