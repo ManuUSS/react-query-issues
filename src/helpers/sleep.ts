a9a3f057da3a961d288fb4206ebc68e71ff616ad
Manuel Ulate <fr3zs09@gmail.com>
2023-09-24 20:13:29 -0600

    sleep helper

---
+++ src/helpers/sleep.ts
@@ -0,0 +1,15 @@
+
+
+export const sleep = ( seconds: number = 1 ):Promise<boolean> => {
+
+    return new Promise(( resolve ) => {
+        
+        setTimeout(() => {
+            
+            resolve( true );
+        
+        }, seconds * 1000 );
+
+    })
+
+}
