b8270d4e18458e38fbd50cb08d7c6bab2125630c
Manuel Ulate <fr3zs09@gmail.com>
2023-09-24 19:43:26 -0600

    react-query devtools setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { router } from './router';
 
 //! Remover enable css source maps
@@ -13,6 +14,7 @@ const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={ client }>
+      <ReactQueryDevtools />
       <RouterProvider router={ router } />
     </QueryClientProvider>
   </React.StrictMode>
