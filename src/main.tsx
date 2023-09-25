3aeeb6e738ea456ac19623697ae57ff93784a55e
Manuel Ulate <fr3zs09@gmail.com>
2023-09-24 19:42:20 -0600

    react-query client config

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './router';
 
 //! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider client={ client }>
+      <RouterProvider router={ router } />
+    </QueryClientProvider>
   </React.StrictMode>
 )
