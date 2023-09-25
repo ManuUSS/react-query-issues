f0620c6d6f9aaab2d7b65e8b5c5e066eea27f992
Manuel Ulate <fr3zs09@gmail.com>
2023-09-24 20:10:44 -0600

    useLabels implemented

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,16 +1,25 @@
-
+import { useLabels } from "../hooks/useLabels"
 
 export const LabelPicker = () => {
 
+  const labelsQuery = useLabels();
+
+  if( labelsQuery.isLoading )
+    return (<h1>Loading...</h1>)
+
   return (
     <div>
-        <span 
+      {
+        labelsQuery.data?.map(({ id, color, name }) => (
+          <span
+            key={ id }
             className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+            style={{ border: `1px solid #${ color }`, color: `#${ color }` }}
+          >
+            { name }
+          </span>
+        ))
+      }
     </div>
   )
 }
