08db874b2b460f9cca2c6f2a6a25a8fefed68ba2
Manuel Ulate <fr3zs09@gmail.com>
2023-09-26 11:04:58 -0600

    fix: headers auth

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
-    headers: {
-        Authorization: 'Bearer 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'
-    }
+    // headers: {
+    //     Authorization: 'Bearer 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'
+    // }
 });
