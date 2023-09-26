3a168bd4b6e84fbb980d177130a30aac6892aa82
Manuel Ulate <fr3zs09@gmail.com>
2023-09-26 11:11:40 -0600

    new: useParams on IssueView

--- src/issues/views/IssueView.tsx
+++ src/issues/views/IssueView.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { IssueComment } from '../components/IssueComment';
 
 const comment1 = "It would provide the ability to create a state, read the state \r\nand set the state form anywhere in the code base.\r\n\r\nIt would be something like this:\r\n\r\n## adding the state to the global state\r\n\r\n```js\r\nimport {useGlobalState} from 'react';\r\nconst ProviderComponent = ()=>{\r\n\r\n  const [ceateState, _, _] = useGlobalState();\r\n\r\n  createState('provider', 'stateName', 'state value');\r\n  createState('provider', 'otherStateName', 'another state value');\r\n  // or maybe, set all the states in one line\r\n  createState('provider', {stateName: 'state value', anotherStateName: 'another state value'});\r\n\r\n  return <></>\r\n}\r\n```\r\n\r\n##  now I can use it like so:\r\n\r\n```js\r\nimport {useGlobalState} from 'react';\r\n\r\nconst ConsumerComponent = ()=>{\r\n  \r\n  const [_, getState, setState] = useGlobalState();\r\n\r\n  const providerStateCpy = getState('key', 'stateName');\r\n\r\n  const changeProviderState = ()=>{\r\n    setState('key', 'stateName', 'new state value');\r\n  }\r\n  return <p onClick={changeProviderState}>{providerStateCpy}</p>\r\n}\r\n```\r\nI wonder if it's a possible thing without making major changes though.";
@@ -7,6 +7,12 @@ const comment3 = "What I don't understand is that in `renderWithHooks`, there is
 
 
 export const IssueView = () => {
+
+  const params = useParams();
+  const { id = '0' } = params;
+
+  const issueQuery = useIssue( id );
+
   return (
     <div className="row mb-5">
       <div className="col-12 mb-3">
