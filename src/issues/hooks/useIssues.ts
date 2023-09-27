966238b632f742ea0cbefc8265b4f6e13523c22f
Manuel Ulate <fr3zs09@gmail.com>
2023-09-27 16:26:34 -0600

    new: sleep implemented on getIssues

--- src/issues/hooks/useIssues.ts
+++ src/issues/hooks/useIssues.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { githubApi } from '../../api/githubApi';
 import { IIssue } from '../interfaces';
+import { sleep } from '../../helpers/sleep';
 
 
 const getIssues = async ():Promise<IIssue[]> => {
-
+    await sleep( 2 );
     const { data } = await githubApi.get<IIssue[]>('/issues')
 
     return data;
