9c8812e5e1a2b25ff697d82e8ab2f9a67eab8f70
Manuel Ulate <fr3zs09@gmail.com>
2023-09-27 16:15:10 -0600

    types node installed

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const configValue : string = process.env.REACT_APP_GIT_TOKEN;
+const configValue : string = process.env.REACT_APP_GIT_TOKEN!;
 
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
