1e977e86036a5acda871a37ad28773a5a83d1255
Manuel Ulate <fr3zs09@gmail.com>
2023-09-28 18:12:55 -0600

    fix: tabs code

--- src/issues/views/ProofForm.tsx
+++ src/issues/views/ProofForm.tsx
@@ -19,26 +19,26 @@ export const ProofForm = () => {
     return (
         <div className="container mt-3">
             <div className="row mt-5">
-            <form noValidate onSubmit={ onSubmitForm }>
-                <AB 
-                    label='Select a drone:' 
-                    value={ object.value }
-                    name='drone'
-                    onChange={ onChange }
-                >
-                <A value='DJI'/>
-                <A value='DJS'/>
-                <A value='DJO'/>
-                <A value='DJP'/>
-                <A value='DJQ'/>
-                <A value='DJR'/>
-                <A value='DJM'/>
-                <A value='DJT'/>
-                </AB>
-                <div className='d-block mt-3'>
-                <input className='btn btn-primary' type="submit" value="Enviar" />
-                </div>
-            </form>
+                <form noValidate onSubmit={ onSubmitForm }>
+                    <AB 
+                        label='Select a drone:' 
+                        value={ object.value }
+                        name='drone'
+                        onChange={ onChange }
+                    >
+                        <A value='DJI'/>
+                        <A value='DJS'/>
+                        <A value='DJO'/>
+                        <A value='DJP'/>
+                        <A value='DJQ'/>
+                        <A value='DJR'/>
+                        <A value='DJM'/>
+                        <A value='DJT'/>
+                    </AB>
+                    <div className='d-block mt-3'>
+                        <input className='btn btn-primary' type="submit" value="Enviar" />
+                    </div>
+                </form>
             </div>
         </div>
       )
