19002177468a9f3a84d864199657871ac230c776
Manuel Ulate <fr3zs09@gmail.com>
2023-09-29 21:06:58 -0600

    new: useIssues interface

--- src/issues/hooks/useIssues.ts
+++ src/issues/hooks/useIssues.ts
@@ -1,15 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { githubApi } from '../../api/githubApi';
-import { IIssue } from '../interfaces';
+import { IIssue, State } from '../interfaces';
 import { sleep } from '../../helpers/sleep';
 
+interface Props {
+    state?: State;
+    labels: string[];
+}
 
 const getIssues = async ():Promise<IIssue[]> => {
     await sleep( 2 );
     const { data } = await githubApi.get<IIssue[]>('/issues')
-
     return data;
-
 }
 
 
