ff3fdbee0cd6f5c46530eea961a66960820aa986
Manuel Ulate <fr3zs09@gmail.com>
2023-10-01 13:29:19 -0600

    fix: components imports - router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { GitApp } from '../GitApp';
+import { IssueView, ListViewInfinite } from '../issues/views';
 
-import { ListView, IssueView, ListViewInfinite } from '../issues/views';
 
 export const router = createBrowserRouter([
   {
